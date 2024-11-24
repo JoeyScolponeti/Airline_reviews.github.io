9533e9949e28b21c3e38c1641b5347181a97320a
karinamehta200219 <144137441+karinamehta200219@users.noreply.github.com>
2024-11-24 14:13:04 -0500

    Add files via upload
    new files with  7 graphs

--- enter.js
+++ enter.js
@@ -1,5 +1,5 @@
 // Set graph margins and dimensions
-var margin = {top: 40, right: 20, bottom: 60, left: 40},
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 500 - margin.left - margin.right,
     height = 250 - margin.top - margin.bottom;
 
@@ -11,7 +11,7 @@ var x = d3.scaleBand()
 var y = d3.scaleLinear()
           .range([height, 0]);
 
-var svg = d3.select("#seat").append("svg")
+var svg3 = d3.select("#enter").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -22,26 +22,26 @@ d3.csv("to50mean.csv").then(function(data) {
 
   // Convert numeric data to numbers
   data.forEach(function(d) {
-    d.Seat_Comfort = +d.Seat_Comfort;
+    d.Inflight_Entertainment = +d.Inflight_Entertainment;
   });
 
   // Scale the range of the data in the domains
   x.domain(data.map(function(d) { return d.Airline; }));
-  y.domain([0, d3.max(data, function(d) { return d.Seat_Comfort; })]);
+  y.domain([0, d3.max(data, function(d) { return d.Inflight_Entertainment; })]);
   
   // Append rectangles for bar chart
-  svg.selectAll(".bar")
+  svg3.selectAll(".bar")
       .data(data)
     .enter().append("rect")
       .attr("class", "bar")
       .attr("x", function(d) { return x(d.Airline); })
       .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.Seat_Comfort); })
-      .attr("height", function(d) { return height - y(d.Seat_Comfort); })
+      .attr("y", function(d) { return y(d.Inflight_Entertainment); })
+      .attr("height", function(d) { return height - y(d.Inflight_Entertainment); })
       .attr("fill", "steelblue"); // Optional: Add color
 
   // Add x axis
-  svg.append("g")
+  svg3.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x))
       .selectAll("text") // Rotate x-axis labels for better readability
@@ -49,15 +49,14 @@ d3.csv("to50mean.csv").then(function(data) {
       .style("text-anchor", "end");
 
   // Add y axis
-  svg.append("g")
+  svg3.append("g")
       .call(d3.axisLeft(y));
 
-      svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
-    .attr("x",0 - (height / 2))
-    .attr("dy", "1em")
-    .style("text-anchor", "middle")
-    .text("Rate");  
+      svg3.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Rate");  
 });
-
--- ground.js
+++ ground.js
@@ -1,5 +1,5 @@
 // Set graph margins and dimensions
-var margin = {top: 40, right: 20, bottom: 60, left: 40},
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 500 - margin.left - margin.right,
     height = 250 - margin.top - margin.bottom;
 
@@ -11,7 +11,7 @@ var x = d3.scaleBand()
 var y = d3.scaleLinear()
           .range([height, 0]);
 
-var svg = d3.select("#seat").append("svg")
+var svg6 = d3.select("#ground").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -22,26 +22,26 @@ d3.csv("to50mean.csv").then(function(data) {
 
   // Convert numeric data to numbers
   data.forEach(function(d) {
-    d.Seat_Comfort = +d.Seat_Comfort;
+    d.Ground_Service = +d.Ground_Service;
   });
 
   // Scale the range of the data in the domains
   x.domain(data.map(function(d) { return d.Airline; }));
-  y.domain([0, d3.max(data, function(d) { return d.Seat_Comfort; })]);
+  y.domain([0, d3.max(data, function(d) { return d.Ground_Service; })]);
   
   // Append rectangles for bar chart
-  svg.selectAll(".bar")
+  svg6.selectAll(".bar")
       .data(data)
     .enter().append("rect")
       .attr("class", "bar")
       .attr("x", function(d) { return x(d.Airline); })
       .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.Seat_Comfort); })
-      .attr("height", function(d) { return height - y(d.Seat_Comfort); })
+      .attr("y", function(d) { return y(d.Ground_Service); })
+      .attr("height", function(d) { return height - y(d.Ground_Service); })
       .attr("fill", "steelblue"); // Optional: Add color
 
   // Add x axis
-  svg.append("g")
+  svg6.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x))
       .selectAll("text") // Rotate x-axis labels for better readability
@@ -49,10 +49,10 @@ d3.csv("to50mean.csv").then(function(data) {
       .style("text-anchor", "end");
 
   // Add y axis
-  svg.append("g")
+  svg6.append("g")
       .call(d3.axisLeft(y));
 
-      svg.append("text")
+      svg6.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 0 - margin.left)
     .attr("x",0 - (height / 2))
@@ -60,4 +60,3 @@ d3.csv("to50mean.csv").then(function(data) {
     .style("text-anchor", "middle")
     .text("Rate");  
 });
-
